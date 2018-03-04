7b57f5fce5367e2703179c169b777fb69d63a2d7
SBB01\tnv <vantamnguyen.vnu@gmail.com>
2018-03-04 16:46:51 +0700

    The second commit to test TeamCity build configuration

--- src/main/java/com/tamco/javademo/DemoApplication.java
+++ src/main/java/com/tamco/javademo/DemoApplication.java
@@ -4,5 +4,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("You have something to show me?");
+        System.out.println("I want to show you something.");
     }
 }
