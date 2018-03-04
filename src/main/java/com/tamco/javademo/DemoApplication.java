31cf97d2b8eaa298d8d1ea38655da05b5b9fb04d
SBB01\tnv <vantamnguyen.vnu@gmail.com>
2018-03-04 16:36:37 +0700

    The first commit to Demo Application

---
+++ src/main/java/com/tamco/javademo/DemoApplication.java
@@ -0,0 +1,8 @@
+package com.tamco.javademo;
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("You have something to show me?");
+    }
+}
